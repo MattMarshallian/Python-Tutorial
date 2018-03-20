6f46d38d4a308c12682be378b9617cdb77f23486
Matt Marshall <matt.marshallian@gmail.com>
2018-03-20 23:21:24 +0100

    string

--- HelloWorld.py
+++ HelloWorld.py
@@ -14,4 +14,15 @@ lines and sentences.
 # Comment
 print(paragraph + '\n\n')
 
-import sys; x = 'foo'; sys.stdout.write(x + '\n')
+import sys;
+
+x = 'foo';
+sys.stdout.write(x + '\n')
+
+my_str = 'Hello, Python!'
+print(my_str)  # Prints complete string
+print(my_str[0])  # Prints first character of the string
+print(my_str[2:5])  # Prints characters starting from 3rd to 5th
+print(my_str[2:])  # Prints string starting from 3rd character
+print(my_str * 2)  # Prints string two times
+print(my_str + "TEST")  # Prints concatenated string
