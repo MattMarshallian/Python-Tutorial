4e89e478cc85dcc4941e664cd2f2b10b4c3062c5
Matt Marshall <matt.marshallian@gmail.com>
2018-03-20 22:58:49 +0100

    basic syntax

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,17 @@
 print('Hello, Python!')
+
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """
+This is a paragraph. It is
+made up of multiple 
+lines and sentences.
+"""
+
+# Comment
+print(paragraph + '\n\n')
+
+import sys; x = 'foo'; sys.stdout.write(x + '\n')
