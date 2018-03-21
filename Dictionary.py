f7633fb6692527eb68f3844ea2bfc397f77f7028
Matt Marshall <matt.marshallian@gmail.com>
2018-03-21 23:27:31 +0100

    list and dictionary

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+contacts = {
+    "Jan": 938477566,
+    "Jacek": 938377264,
+    "Janusz": 947662781
+}
+
+for a, b in contacts.items():
+    print("{0} - {1}".format(a, b))
---
+++ List.py
@@ -0,0 +1,17 @@
+print("File List.py")
+
+mylist = ['a', 'b', 'c', 'd', 'r']
+s = '.'
+
+print(s.join(mylist))
+
+bar = 'Zdrabniamy literki'
+
+for i in bar:
+    print (i)
+
+bar = ['foo', 'bar', 'zzyc']
+
+for i in bar:
+    print (i)
+
